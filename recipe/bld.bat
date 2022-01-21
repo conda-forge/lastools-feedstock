a1062be08ed0e20296f1252f5969c14019280867
Sam Gillingham <gillingham.sam@gmail.com>
2022-01-21 11:30:49 +1000

    fix shared lib names and attempt to fix Windows link

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ cd build
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D BUILD_SHARED_LIBS=ON ^
+    -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
     ..
 if errorlevel 1 exit 1
 
